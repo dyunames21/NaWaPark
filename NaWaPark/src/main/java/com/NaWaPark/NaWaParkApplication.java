7b40d875e0eb8fafc75a6e1e01c29d23df2ebe29
박진형 <dyunames21@gmail.com>
2024-05-08 14:51:37 +0900

    [init] HelloController Test

---
+++ NaWaPark/src/main/java/com/NaWaPark/NaWaParkApplication.java
@@ -0,0 +1,13 @@
+package com.NaWaPark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaWaParkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaWaParkApplication.class, args);
+	}
+
+}
---
+++ NaWaPark/src/test/java/com/NaWaPark/NaWaParkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NaWaPark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaWaParkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
